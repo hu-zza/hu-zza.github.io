5a499ca3e0165d12d33020b53a2be6f48e45148a
Szabó László András <48407681+hu-zza@users.noreply.github.com>
2021-03-03 19:46:48 +0100

    Featherlight modal, redesigned mobile menu

--- js/main.js
+++ js/main.js
@@ -4,45 +4,43 @@ sitemap:
   exclude: 'yes'
 ---
 
+function toggleMobileView() {
+  $('.btn-mobile-menu').toggleClass('visible');
+  $('.navigation-wrapper').toggleClass('visible');
+}
+
+function toggleTimeline() {
+  if ($('.content-wrapper').hasClass('showing')){
+    $('.content-wrapper').removeClass('animated slideInRight');
+    $('.panel-cover').removeClass('panel-cover--collapsed');
+    $('.panel-cover').css('max-width', '100%');
+    $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {});
+    $('.content-wrapper').removeClass('showing');
+    return;
+  }
+  currentWidth = $('.panel-cover').width();
+  if (currentWidth < 960) {
+    $('.panel-cover').addClass('panel-cover--collapsed');
+    $('.content-wrapper').addClass('animated slideInRight');
+  } else {
+    $('.panel-cover').css('max-width', currentWidth);
+    $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {});
+  }
+  $('.content-wrapper').addClass('showing');
+}
+
 $(document).ready(function () {
-  $('a.panel-button').click(function (e) {
+  $.featherlight.defaults.closeOnClick = 'anywhere';
+  $.featherlight.defaults.closeIcon = '';
+  $.featherlight.defaults.openSpeed = 350;
+  $.featherlight.defaults.closeSpeed = 350;
+
+  $('.panel-button').click(function (e) {
       e.preventDefault();
-    //if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    if ($('.content-wrapper').hasClass('showing')){
-      $('.content-wrapper').removeClass('animated slideInRight')
-      $('.panel-cover').removeClass('panel-cover--collapsed')
-      $('.panel-cover').css('max-width', '100%')
-      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
-      $('.content-wrapper').removeClass('showing')
-      return;
-    }
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-    }
-    $('.content-wrapper').addClass('showing');
+      toggleTimeline();
+      if (currentWidth < 960) {
+          toggleMobileView();
+      }
   })
 
-  $('.btn-mobile-menu').click(function () {
-    if ($('.content-wrapper').hasClass('showing')){
-        $('.content-wrapper').removeClass('animated slideInRight')
-        $('.panel-cover').removeClass('panel-cover--collapsed')
-        $('.panel-cover').css('max-width', '100%')
-        $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
-        $('.content-wrapper').removeClass('showing')
-    } else {
-        $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    }
-
-    $('.btn-mobile-menu__icon').toggleClass('hidden')
-    $('.btn-mobile-close__icon').toggleClass('hidden')
-  })
-
-  $('.navigation-wrapper .projects-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-  })
 })
