7ae51f3dc74a717e348ecdba4108df82c0b91c30
Szabó László András <48407681+hu-zza@users.noreply.github.com>
2021-03-03 02:42:12 +0100

    Adjust favicons and background cover, delete foundation icons.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ sitemap:
 
 $(document).ready(function () {
   $('a.panel-button').click(function (e) {
+      e.preventDefault();
     //if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     if ($('.content-wrapper').hasClass('showing')){
       $('.content-wrapper').removeClass('animated slideInRight')
@@ -13,8 +14,6 @@ $(document).ready(function () {
       $('.panel-cover').css('max-width', '100%')
       $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
       $('.content-wrapper').removeClass('showing')
-      window.location.hash = '';
-      parent.location.hash = ''
       return;
     }
     currentWidth = $('.panel-cover').width()
@@ -27,14 +26,6 @@ $(document).ready(function () {
     }
     $('.content-wrapper').addClass('showing');
   })
-
-  if (window.location.hash && window.location.hash == '#projects') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
