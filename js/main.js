084638fce09020f8b8826ccf2508ff8e66b9bded
Szabó László András <48407681+hu-zza@users.noreply.github.com>
2021-03-06 21:11:29 +0100

    Mostly graphical changes
    Add missing 'fa-lg' class to some Font Awesome icons.
    Adjust copyright footer (break into two lines + line-height).
    Adjust responsive parameters.
    Extend _config.xml -> authors description + highlight
    
    Simplify main.js, delete jquery methods (css, animate),
    so now css is only responsible for the animations.

--- js/main.js
+++ js/main.js
@@ -4,35 +4,31 @@ sitemap:
   exclude: 'yes'
 ---
 
-$(window).resize(function() {
-    if ($('.content-wrapper').hasClass('showing')){
-        toggle();
-    }
-});
-
 $(document).ready(function () {
     $.featherlight.defaults.closeOnClick = 'anywhere';
     $.featherlight.defaults.closeIcon = '';
     $.featherlight.defaults.openSpeed = 350;
     $.featherlight.defaults.closeSpeed = 350;
 
-    $('.panel-button').click(function (e) {
+    $('.projects-button').click(function (e) {
         e.preventDefault();
         toggle();
     });
 
     waitTextStart = "<a href='#' title='Wait a bit...' target='_blank'><i class='";
+
     waitTextEnd = "'></i><span class='label'>Wait a bit...</span></a>";
+
     result = [ "=", "=", "g", "P", "h", "9", "C", "P", "+", "4", "W", "Y", "w", "N", "3", "L", "8", "w", "W", "a", "h", "1", "W", "R", "+", "c", "C", "b", "l", "J", "W", "Y", "s", "d", "S", "P", "z", "N", "X", "Y", "s", "N", "G", "I", "u", "F", "G", "c", "z", "x", "j", "P", "p", "9", "C", "P", "+", "c", "y", "Z", "s", "1", "S", "Y", "m", "B", "S", "Z", "w", "9", "G", "b", "l", "Z", "n", "b", "l", "1", "S", "Y", "m", "B", "y", "c", "h", "Z", "2", "J", "9", "M", "3", "c", "h", "x", "2", "Y", "g", "k", "G", "P", "+", "c", "y", "a", "u", "F", "G", "b", "i", "9", "1", "J", "9", "Q", "X", "Z", "n", "J", "X", "Y", "0", "B", "y", "J", "1", "h", "m", "L", "h", "p", "n", "e", "A", "R", "W", "Z", "0", "N", "W", "Z", "0", "9", "m", "c", "w", "B", "C", "b", "p", "F", "W", "b", "F", "d", "S", "P", "l", "x", "G", "d", "p", "R", "H", "I", "n", "U", "H", "a", "u", "E", "m", "e", "6", "B", "E", "Z", "l", "R", "3", "Y", "l", "R", "3", "b", "y", "B", "n", "O", "v", "R", "H", "b", "p", "F", "W", "b", "n", "0", "j", "Z", "l", "J", "H", "a", "g", "E", "G", "P" ];
 
-    $("#id").html(waitTextStart + "fas fa-hourglass-start" + waitTextEnd);
+    $("#id").html(waitTextStart + "fas fa-hourglass-start fa-lg" + waitTextEnd);
 
     setTimeout(function() {
-        $("#id").html(waitTextStart + "fas fa-hourglass-half" + waitTextEnd);
+        $("#id").html(waitTextStart + "fas fa-hourglass-half fa-lg" + waitTextEnd);
     }, 700);
 
     setTimeout(function() {
-        $("#id").html(waitTextStart + "fas fa-hourglass-end" + waitTextEnd);
+        $("#id").html(waitTextStart + "fas fa-hourglass-end fa-lg" + waitTextEnd);
     }, 1400);
 
     setTimeout(function() {
@@ -41,36 +37,30 @@ $(document).ready(function () {
 })
 
 function toggle() {
+    toggleTimeline();
+    toggleMobileView();
+}
+
+function toggleTimeline() {
     if ($('.content-wrapper').hasClass('showing')){
         closeTimeline();
+        $('.content-wrapper').removeClass('showing');
     } else {
         openTimeline();
+        $('.content-wrapper').addClass('showing');
     }
 }
 
 function openTimeline() {
-    currentWidth = $('.panel-cover').width();
-
-    if (currentWidth <= 960) {
-        $('.panel-cover').addClass('panel-cover--collapsed');
-        $('.content-wrapper').addClass('animated slideInRight');
-        toggleMobileView();
-    } else {
-        $('.panel-cover').css('max-width', currentWidth);
-        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {});
-    }
-    $('.content-wrapper').addClass('showing');
+    $('.content-wrapper').removeClass('slideOutRight');
+    $('.content-wrapper').addClass('slideInRight');
+    $('.panel-cover').addClass('panel-cover--collapsed');
 }
 
 function closeTimeline() {
-    $('.content-wrapper').removeClass('animated slideInRight');
+    $('.content-wrapper').removeClass('slideInRight');
+    $('.content-wrapper').addClass('slideOutRight');
     $('.panel-cover').removeClass('panel-cover--collapsed');
-    $('.panel-cover').css('max-width', '100%');
-    $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {});
-    $('.content-wrapper').removeClass('showing');
-    if ($('.btn-mobile-menu').hasClass('visible')) {
-        toggleMobileView();
-    }
 }
 
 function toggleMobileView() {
